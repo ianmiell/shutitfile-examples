aefddb08f8db340baed49706ec601aed8cd59bfe
ianmiell <ian.miell@gmail.com>
2015-12-08 19:35:54 +0000

    broken

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # shutitfile-examples
 For Vaidotas
+
+See run.sh
+
+working should work, broken will fail at the test stage
--- run.sh
+++ run.sh
@@ -2,4 +2,13 @@ rm -rf tmpbuild
 shutit skeleton --dockerfiles $(pwd)/working/a.sif $(pwd)/working/b.sif --module_directory $(pwd)/tmpbuild --module_name testing --domain shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04 --delivery docker
 cd tmpbuild/bin
 ./build.sh
+cd -
+
+
+rm -rf tmpbuild
+shutit skeleton --dockerfiles $(pwd)/broken/a.sif $(pwd)/broken/b.sif --module_directory $(pwd)/tmpbuild --module_name testing --domain shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04 --delivery docker
+cd tmpbuild/bin
+./build.sh
+cd -
+
 
